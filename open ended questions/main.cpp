20291eae275633caa8616e26ea4aa490ae0e8768
revanthr877 <53810148+revanthr877@users.noreply.github.com>
2020-12-30 14:21:59 +0530

    Add files via upload

---
+++ open ended questions/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printUnique(int l, int r)
+{
+
+    for (int i=l ; i<=r ; i++)
+    {
+
+        int num = i;
+        bool visited[10] = {false};
+
+        while (num)
+        {
+            if (visited[num % 10])
+                break;
+
+            visited[num%10] = true;
+            num = num/10;
+
+        }
+
+        if (num == 0)
+            cout << i << " ";
+    }
+}
+
+
+
+int main()
+{
+
+    int l,r;
+
+    cout << "L:";
+    cin >> l;
+
+    cout << "R:";
+    cin >> r;
+
+    printUnique(l, r);
+
+    return 0;
+}
